3c0e3f3a2ee24ecfd176b685ffc26898042192fd
Tracyszhao <tracyszhao@gmail.com>
2026-09-07 00:34:05 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Minimal offline cache. Bump CACHE when you change files so users get updates. */
-const CACHE = 'track-v18';
+const CACHE = 'track-v19';
 const ASSETS = [
   './',
   './index.html',
